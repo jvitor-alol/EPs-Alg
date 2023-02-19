3f1c475cecc3a63b9834ec2f4c3147fa88b2f1e8
jvitor-alol <jvitor.alol@protonmail.com>
2023-02-19 17:02:52 -0300

    Commit

--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -1,5 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine(3+4);
-int num = int.Parse(Console.ReadLine());
-Console.Write("{0}", num);
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(3 + 4);
+        int num = int.Parse(Console.ReadLine());
+        Console.Write(num);
+    }
+}
