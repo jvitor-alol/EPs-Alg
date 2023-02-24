5fe0547a9dc1b5018a48851f6c64304ef770b9f6
jvitor-alol <jvitor.alol@protonmail.com>
2023-02-24 20:18:04 -0300

    gasolinaAlcoolFinal

--- gasolinaAlcool/Program.cs
+++ gasolinaAlcool/Program.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 internal class Program
 {
-    public static int maiorMenor(float razaoReal){
+    public static void maiorMenor(float razaoReal){
         const float razao = 0.7F;
 
         if (razaoReal > razao) {
-            return 0;
+            Console.WriteLine("Vale a pena a gasolina");
         }else if (razaoReal < razao){
-                return 1;
-            } else return 2;
+                Console.WriteLine("Vale a pena o álcool");
+            } else Console.WriteLine("Tanto faz abastecer com um ou outro");
     }
     public static void Main(string[] args)
     {
         string simNao = "";
         
         do{
+            Console.Clear();
             Console.Write("Insira o preço da gasolina: R$ ");
             float gasolina = float.Parse(Console.ReadLine());
             Console.Write("Insira o preço do álcool: R$ ");
@@ -22,17 +23,7 @@ internal class Program
 
             float razaoReal = alcool / gasolina;
 
-            switch (maiorMenor(razaoReal)){
-                case 0:
-                    Console.WriteLine("Vale a pena a gasolina");
-                    break;
-                case 1:
-                    Console.WriteLine("Vale a pena o álcool");
-                    break;
-                case 2:
-                    Console.WriteLine("Tanto faz abastecer com um ou outro");
-                    break;
-            }
+            maiorMenor(razaoReal);
 
             Console.Write("Deseja iniciar outro cálculo? [S/N]: ");
             simNao = Console.ReadLine();
@@ -40,6 +31,6 @@ internal class Program
                 Console.WriteLine("Valor inválido, insira novamente");
                 simNao = Console.ReadLine();
             }
-        } while(simNao == "s" | simNao == "S");
+        } while (simNao == "s" | simNao == "S");
     }
 }
