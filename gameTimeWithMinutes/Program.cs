30910a2cc5c19db58dab46327a5f2be56ced7b82
jvitor-alol <jvitor.alol@protonmail.com>
2023-02-24 20:05:26 -0300

    gameTime / gasolinaAlcool

--- gameTimeWithMinutes/Program.cs
+++ gameTimeWithMinutes/Program.cs
@@ -1,101 +1,26 @@
 ﻿using System;
 public class Program
 {
-    /*public int hourDur, minuteDur;
-    private static int validGame (int startHour, int startMinute, int endHour, int endMinute)
-    {   
-        int i;
-
-        if (startHour < endHour & endMinute <= startMinute){
-            Console.WriteLine("Jogo inválido");
-            Environment.Exit(0);
-        }
-
-        if (startHour >= endHour)
-
-        return i;
-        if (startHour == endHour)
-        {
-            if (startMinute == endMinute)
-            {
-                hourDur = 24;
-                minuteDur = 0;                
-            } else if (startMinute > endMinute)
-                {
-                    hourDur = 23;
-                    minuteDur = 60 - Math.Abs(startMinute - endMinute);
-                } else 
-                {
-                    hourDur = 0;
-                    minuteDur = endMinute - startMinute;
-                }
-            //return true;
-        } else if (startHour > endHour)
-        {
-            hourDur = 24 - Math.Abs(startHour - endHour);
-            if (startMinute > endMinute) 
-            {
-                minuteDur = 60 - Math.Abs(endMinute - startMinute);
-            } else if (startMinute < endMinute)
-                {
-                    minuteDur = endMinute - startMinute;
-                } else minuteDur = 0;
-            //return true;
-        } else {
-            if (endMinute <= startMinute) {
-                //return false;
-            } else {
-                hourDur = 0;
-                minuteDur = endMinute - startMinute;
-                //return true;
-            }
-        }
-    }*/
-
     private static void Main(string[] args)
     {
-            Console.Write("Entradas: ");
-            string[] texto = Console.ReadLine().Split(' ');
+        //Console.Write("Entradas: ");
+        string[] texto = Console.ReadLine().Split(' ');
 
-            int startHour = int.Parse(texto[0]), startMinute = int.Parse(texto[1]), endHour = int.Parse(texto[2]), endMinute = int.Parse(texto[3]);
+        int startGame = int.Parse(texto[0])*60 + int.Parse(texto[1]), endGame = int.Parse(texto[2])*60 + int.Parse(texto[3]);
+        int hourDur, minDur;
 
-            int hourDur, minuteDur;
-
-            if (startHour == endHour)
+        if(endGame>startGame)
+        {
+            hourDur = (endGame - startGame)/60;
+            minDur = (endGame - startGame)%60;
+            
+            Console.WriteLine("O JOGO DUROU {0} HORA(S) E {1} MINUTO(S)", hourDur, minDur);
+        } else if(endGame<startGame)
             {
-                if (startMinute == endMinute)
-                {
-                    hourDur = 24;
-                    minuteDur = 0;
-                } else if (startMinute > endMinute)
-                    {
-                        hourDur = 23;
-                        minuteDur = 60 - Math.Abs(startMinute - endMinute);
-                    } else 
-                    {
-                        hourDur = 0;
-                        minuteDur = endMinute - startMinute;
-                    }
-            } else if (startHour > endHour)
-            {
-                hourDur = 24 - Math.Abs(startHour - endHour);
-                if (startMinute > endMinute) 
-                {
-                    minuteDur = 60 - Math.Abs(endMinute - startMinute);
-                } else if (startMinute < endMinute)
-                    {
-                        minuteDur = endMinute - startMinute;
-                    } else minuteDur = 0;
-            } else {
-                if (endMinute <= startMinute) {
-                    Console.WriteLine("Jogo inválido");
-                    Environment.Exit(0);
-                } else {
-                    hourDur = 0;
-                    minuteDur = endMinute - startMinute;
-                }
-            }
+                hourDur = (24*60-startGame+endGame)/60;
+                minDur =  (24*60-startGame+endGame)%60;
 
-        Console.WriteLine("O JOGO DUROU {0} HORA(S) E {1} MINUTO(S)", hourDur, minuteDur);       
+                Console.WriteLine("O JOGO DUROU {0} HORA(S) E {1} MINUTO(S)", hourDur, minDur);
+            } else Console.WriteLine("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)");
     }
 }
--- gasolinaAlcool/Program.cs
+++ gasolinaAlcool/Program.cs
@@ -1,8 +1,45 @@
 ﻿using System;
 internal class Program
 {
-    private static void Main(string[] args)
+    public static int maiorMenor(float razaoReal){
+        const float razao = 0.7F;
+
+        if (razaoReal > razao) {
+            return 0;
+        }else if (razaoReal < razao){
+                return 1;
+            } else return 2;
+    }
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string simNao = "";
+        
+        do{
+            Console.Write("Insira o preço da gasolina: R$ ");
+            float gasolina = float.Parse(Console.ReadLine());
+            Console.Write("Insira o preço do álcool: R$ ");
+            float alcool = float.Parse(Console.ReadLine());
+
+            float razaoReal = alcool / gasolina;
+
+            switch (maiorMenor(razaoReal)){
+                case 0:
+                    Console.WriteLine("Vale a pena a gasolina");
+                    break;
+                case 1:
+                    Console.WriteLine("Vale a pena o álcool");
+                    break;
+                case 2:
+                    Console.WriteLine("Tanto faz abastecer com um ou outro");
+                    break;
+            }
+
+            Console.Write("Deseja iniciar outro cálculo? [S/N]: ");
+            simNao = Console.ReadLine();
+            while (simNao != "s" & simNao != "S" & simNao != "n" & simNao != "N"){
+                Console.WriteLine("Valor inválido, insira novamente");
+                simNao = Console.ReadLine();
+            }
+        } while(simNao == "s" | simNao == "S");
     }
 }
